ab53de7102afd332296a2730e5e5f5187de57cfe
Ilya German <ilya.german@gmail.com>
2019-06-27 20:54:22 +0300

    Initial Commit

---
+++ Ilya_Swayder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ilya_Swayder
+//
+//  Created by ilya_admin on 27/06/2019.
+//  Copyright © 2019 ilya_admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
